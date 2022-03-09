84e29e449666a1032c728a2646ed9af0fc173cde
CWYRenee <chiuwingyin@gmail.com>
2022-03-08 17:22:05 -0800

    finished generic header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,13 @@
-
 import React from "react";
 import "./Header.css";
-import logo from './out_logo.svg';
+import logo from "./out_logo.svg";
 import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import HeaderOption from "./HeaderOption";
 
 function Header() {
   return (
@@ -18,7 +23,14 @@ function Header() {
         </div>
       </div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption avatar={true} title="Log Out" />
+      </div>
     </div>
   );
 }
