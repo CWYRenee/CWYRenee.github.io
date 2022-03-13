8059d71eaa988a27214b4d9334e6a1c57b7172ab
CWYRenee <chiuwingyin@gmail.com>
2022-03-12 16:52:42 -0800

    added background colour

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header.js';
 import Profile from './components/Feed/Profile';
+import Projects from './components/Feed/Projects';
 
 function App() {
   return (
     <div className="app">
     <Header />
     <Profile />
+    <Projects />
     </div>
   );
 }
--- src/components/Feed/Projects.js
+++ src/components/Feed/Projects.js
@@ -1,8 +1,27 @@
+import { Card, CardActionArea } from '@material-ui/core'
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@mui/material/CardActions';
+import { Typography } from '@material-ui/core';
+import { Avatar } from '@material-ui/core';
+
 import React from 'react'
+import "./Projects.sass";
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <Card className='projects' >
+    <CardActionArea className='projects_action' >
+      <CardContent className="profile_content">
+        <Typography gutterBottom variant="h5" component="h5" style={{marginLeft: "20px", fontWeight: "bolder"}}>
+          Renee Chiu
+        </Typography>
+        <Typography variant="body1" color="textSecondary" component="h6" style={{marginLeft: "20px", fontWeight: "bold"}}>
+          Major in Management, Minor in Computer Science
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+  </Card>
   )
 }
 
