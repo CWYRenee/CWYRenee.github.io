e87eb0f8fcfa7ad13fdaf153a3cca1083107baa2
CWYRenee <chiuwingyin@gmail.com>
2022-03-13 16:41:15 -0700

    initial education info

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header/Header.js';
 import Profile from './components/Feed/Profile';
 import Projects from './components/Feed/Projects';
+import Education from './components/Feed/Education';
 
 function App() {
   return (
     <div className="app">
     <Header />
     <Profile />
+    <Education />
     <Projects />
     </div>
   );
--- src/components/Feed/Education.js
+++ src/components/Feed/Education.js
@@ -1,8 +1,35 @@
+import { Card, CardActionArea } from '@material-ui/core'
+import CardContent from '@material-ui/core/CardContent';
+import { Typography } from '@material-ui/core';
+
 import React from 'react'
+import "./Education.sass";
 
 function Education() {
   return (
-    <div>Education</div>
+    <Card className='education' >
+    <CardContent className="education_content" style={{marginLeft: "20px"}}>
+      <Typography gutterBottom variant="h5" component="div" style={{fontWeight: "bold", marginBottom: "20px"}}>
+      Education
+      </Typography>
+      
+      <CardActionArea className='ind_edu' href='https://www.ubc.ca/' target='_blank'>
+      <Typography gutterBottom variant="h6" component="div" style={{marginTop: "20px"}}>
+      The University of British Columbia
+      </Typography>
+      <Typography variant="subtitle2" gutterBottom component="div" style={{marginBottom: "10px"}}>
+      Bachelor's of Management with Minor in Computer Science
+      </Typography>
+      <Typography variant="subtitle2" gutterBottom component="div" style={{marginBottom: "10px"}}>
+      2018 - 2022
+      </Typography>
+      <Typography variant="body1" gutterBottom style={{marginBottom: "20px"}}>
+      Outstanding Internatioanl Student Award recipient
+      </Typography>
+      </CardActionArea>
+
+    </CardContent>
+  </Card>
   )
 }
 
