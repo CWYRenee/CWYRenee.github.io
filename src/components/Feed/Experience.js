b16d2da61e949169fc80e3ffac9d6312f8870178
CWYRenee <chiuwingyin@gmail.com>
2022-03-15 12:04:58 -0700

    smart meals exp add

--- src/components/Feed/Experience.js
+++ src/components/Feed/Experience.js
@@ -1,39 +1,36 @@
 import { Card, CardActionArea } from '@material-ui/core'
 import CardContent from '@material-ui/core/CardContent';
 import { Typography } from '@material-ui/core';
+import CardMedia from '@material-ui/core/CardMedia';
 
 import React from 'react'
 import "./Experience.sass";
+import equifood_logo from './EquiFoodlogo.png'
 
 function Experience() {
   return (
     <Card className='experience' >
     <CardContent className="exp_content" style={{marginLeft: "20px"}}>
       <Typography gutterBottom variant="h5" component="div" style={{fontWeight: "bold", marginBottom: "20px"}}>
-      Projects
-      </Typography>
+      Experience
+      </Typography>   
       
-      <CardActionArea className='ind_exp' href='https://cwyrenee.github.io/chromaticpuzzle/' target='_blank'>
-      <Typography gutterBottom variant="h6" component="div" style={{marginTop: "20px"}}>
-      Flutter Puzzle Hack
-      </Typography>
-      <Typography variant="subtitle2" gutterBottom component="div" style={{marginBottom: "10px"}}>
-      Feb 2022 - Mar 2022
-      </Typography>
-      <Typography variant="body1" gutterBottom style={{marginBottom: "20px"}}>
-      A music inspired slide puzzle based on the C Chromatic scale. 
-      </Typography>
-      </CardActionArea>
-      
-      <CardActionArea className='ind_project' href='https://docs.google.com/presentation/d/1ppivDnU28mpybBk6xVdqU3hwaAc0zkD6iPj7o6sRxCw/edit?usp=sharing' target='_blank'>
+      <CardMedia
+          className= "equifood_logo"
+          component="img"
+          src={equifood_logo}
+        />
+      <CardActionArea className='ind_exp' href='https://www.projectequifood.com/?fbclid=IwAR1XlLTc6kK0RprCf-2e9SzhIYYrJP2Jz7H9zO4m_B4I6XaLSOHFYRBp2YQ' target='_blank' style={{marginLeft: '80px'}}>
       <Typography gutterBottom variant="h6" component="div">
-      Creating a Sustainable Community- NextGen Consulting Ltd.
+      Executive Project Member
       </Typography>
       <Typography variant="subtitle2" gutterBottom component="div" style={{marginBottom: "10px"}}>
-      2 weeks | Jan 2021
+      9 mos | Jul 2020 - Mar 2021
       </Typography>
       <Typography variant="body1" gutterBottom style={{marginBottom: "20px"}} >
-      A HR solution to rekindle a firm’s commitment to corporate social responsibility in a case competition environment (Cut to the Case- HRMC UBC).
+      • Assisted in developing a scalable donation system in engaging businesses to promote food security <br />
+      • Reached out to local businesses for a partnership opportunity <br />
+      • Planned and created marketing contents tailored to partners' business <br />
       </Typography>
       </CardActionArea>
 
