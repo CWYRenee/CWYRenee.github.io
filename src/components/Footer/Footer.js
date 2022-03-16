3be59c424370a5338a366d3f32271f9a05893098
CWYRenee <chiuwingyin@gmail.com>
2022-03-16 13:35:42 -0700

    formatted footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,20 @@
+import { Typography } from '@material-ui/core';
+
 import React from 'react'
 import './Footer.sass'
 
 function Footer() {
   return (
     <div className="footer">
-        
+        <Typography className='footer_content' variant="subtitle2" gutterBottom >
+        <div className="footer_left"><a href="./Profile">Contact Me </a></div>
+        |
+        <div className="footer_right"><a href='' target='_blank'>Why LinkedOut?</a></div>
+        </Typography>
+
+        <Typography className='footer_content' variant="subtitle2" gutterBottom >
         © 2022 Renee Chiu
+        </Typography>
     </div>
   )
 }
