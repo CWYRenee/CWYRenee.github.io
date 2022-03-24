5cbf7fc150a281ba7886d119a06645c3717cd420
CWYRenee <chiuwingyin@gmail.com>
2022-03-23 21:35:13 -0700

    linked ContactCard to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,26 @@
 import { Typography } from '@material-ui/core';
+import Modal from '../Feed/Modal'
+import ContactCard from "../Feed/ContactCard";
 
-import React from 'react'
+import React, {useRef} from 'react'
 import './Footer.sass'
+import { Button } from '@mui/material';
 
 const Footer = () => {
+  
+  const modal = useRef(null)
+  
   return (
     <div className="footer">
         <Typography className='footer_content' variant="subtitle2" gutterBottom >
-        <div className="footer_left"><a href="./Profile">Contact Me </a></div>
-        |
-        <div className="footer_right"><a href='' target='_blank' rel="noopener">Why LinkedOut?</a></div>
+        <div className="footer_left"><Button onClick={() => {modal.current.open()}} style={{ color:"#bf360c"}}>Contact Me </Button></div>
+        
+        <div className="footer_right"><Button target='_blank' rel="noopener" style={{ color:"#bf360c"}}>Why LinkedOut?</Button></div>
         </Typography>
-
+        <Modal ref={modal}>
+          <ContactCard />
+        </Modal>
+        
         <Typography className='footer_content' variant="subtitle2" gutterBottom >
         © 2022 Renee Chiu
         </Typography>
